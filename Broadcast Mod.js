06af222ff09ae7ddff1634f0a79f3395c740ff9d
dli417 <dli417@users.noreply.github.com>
2022-03-05 05:03:52 +0000

    Update Broadcast Mod.js

--- Broadcast Mod.js
+++ Broadcast Mod.js
@@ -1,4 +1,4 @@
-t.prefs_.set('color-palette-overrides',["#000000", "#be2d26", "#519f50", "#e99d2a", "#5a86ad", "#ac80a6", "#74a6ad", "#ffffff", "#323232", "#e84627", "#83d182", "#d0d150", "#b8d3ed", "#d19ecb", "#93cfd7", "#ffffff"]);
+t.prefs_.set('color-palette-overrides',["#000000", "#be2d26", "#519f50", "#e99d2a", "#5a86ad", "#ac80a6", "#74a6ad", "#bfbfbf", "#323232", "#e84627", "#83d182", "#fecd22", "#b8d3ed", "#d19ecb", "#93cfd7", "#f5f5f5"]);
 t.prefs_.set('foreground-color', "#e6e1dc");
 t.prefs_.set('background-color', "#2b2b2b");
 t.prefs_.set('cursor-color', 'rgba(255,255,255,0.5)');
